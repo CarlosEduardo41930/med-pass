9120b129be20a94b3612bddf4f722ae9ec56ece5
CarlosEduardo41930 <carlos41930@fiec.edu.br>
2026-03-18 21:15:53 -0300

    colocando código backend

--- medicamento_uso.php
+++ medicamento_uso.php
@@ -1,18 +1,26 @@
+<?php
+require "AuthController.php";
+
+$medicamento = Medicamento($conexao);
+
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Medicamentos em Uso</title>
-  <link rel="stylesheet" href="estilo.css">
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
+
 <body>
 
   <!-- Top bar -->
   <div class="topbar">
-    <a href="#" class="topbar-icon"> 
-      <i class="fa-solid fa-house"></i> 
+    <a href="#" class="topbar-icon">
+      <i class="fa-solid fa-house"></i>
     </a>
     <img src="https://i.postimg.cc/VSSzvwD8/Med-Pass-Logo-(resolucao-maior).png" alt="Logo MedPass" class="logo-img" />
     <a href="#" class="topbar-icon">
@@ -42,26 +50,13 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Osimertinib (Tagrisso)</td>
-          <td>80 mg</td>
-          <td>1 Comprimido, uma vez ao dia (Diário)</td>
-        </tr>
-        <tr>
-          <td>Pemetrexede</td>
-          <td>500 mg/m²</td>
-          <td>A cada 21 dias</td>
-        </tr>
-        <tr>
-          <td>Insulina Glargina (Lantus)</td>
-          <td>14 unidades</td>
-          <td>1 Vez ao dia</td>
-        </tr>
-        <tr>
-          <td>Insulina Asparte (Novorapid)</td>
-          <td>Variável, calculada por bomba</td>
-          <td>1U a cada 10g de Carboidrato</td>
-        </tr>
+        <?php foreach ($medicamento as $remedio) : ?>
+          <tr>
+            <td><?= $remedio['nome']; ?></td>
+            <td><?= $remedio['dosagem']; ?></td>
+            <td><?= $remedio['frequencia']; ?></td>
+          </tr>
+        <?php endforeach; ?>
       </tbody>
     </table>
 
@@ -99,4 +94,5 @@
 
   </div>
 </body>
+
 </html>
